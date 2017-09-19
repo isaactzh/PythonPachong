2f004ba1dcbd6d2df759e685ef3b3f091c582ed0
isaactzh <isaactzh0102@gmail.com>
2017-09-19 23:01:40 +0800

    hw2

--- python_爬虫实战/hw2_分析Ajax请求抓图/spider.py
+++ python_爬虫实战/hw2_分析Ajax请求抓图/spider.py
@@ -6,8 +6,14 @@ from bs4 import BeautifulSoup
 import re
 import pymongo
 from config import *
+import os
+from hashlib import md5
+from multiprocessing import Pool
 
-client = pymongo.MongoClient(MONGO_URL)
+
+client = pymongo.MongoClient(MONGO_URL, connect = False)
+db = client[MONGO_DB]
+
 def get_page_index(offset, keyword):
     data = {
         'offset': offset,
@@ -18,9 +24,11 @@ def get_page_index(offset, keyword):
         'cur_tab': 3
     }
     url = 'https://www.toutiao.com/search_content/?' + urlencode(data)
+    print('this is url ', url)
     try:
         response = requests.get(url)
         if response.status_code == 200:
+            #print(response.text)
             return response.text
         return None
     except RequestException:
@@ -55,20 +63,52 @@ def parse_page_detial(html, url):
         if data and 'sub_images' in data.keys():
             sub_images = data.get('sub_images')
             images = [item.get('url') for item in sub_images]
+            for image in images: download_image(image)
             return {
                 'title': title,
                 'url': url,
                 'images': images
             }
 
-def main():
-    html = get_page_index(0, '街拍')
+def save_to_mongo(result):
+    if db[MONGO_TABLE].insert(result):
+        print('存储到MONGODB成功', result)
+        return True
+    return False
+
+def download_image(url):
+    print('正在下载', url)
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            save_image(response.content) #content返回2进制
+        return None
+    except RequestException:
+        print('请求图片出错', url)
+        return None
+
+def save_image(content):
+    file_path = '{0}/{1}.{2}'.format(os.getcwd(), md5(content).hexdigest(), 'jpg')
+    if not os.path.exists(file_path):
+        with open(file_path, 'wb') as f:
+            f.write(content)
+            f.close()
+
+
+def main(offset):
+    html = get_page_index(offset, KEYWORLD)
     for url in parse_page_index(html):
         html = get_page_detail(url)
         if html:
             result = parse_page_detial(html, url)
-            print(result)
+            if result:
+                save_to_mongo(result)
+            else:
+                print('不正确的网页格式')
         #break
 
 if __name__ == '__main__':
-    main() #返回的是json数据
+    #main(0) #返回的是json数据
+    groups = [x*20 for x in range(GROUP_START, GROUP_END+1)]
+    pool = Pool()
+    pool.map(main, groups)
